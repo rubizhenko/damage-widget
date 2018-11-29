1b7d1e2194b67792f0396b7a9103fe39dd66b76d
Dmytro Rubizhenko <d.rubizhenko@gmail.com>
2018-11-29 16:47:28 +0200

    add first svg car

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,1 +1,32 @@
-//Some JS
+const Damages = (function() {
+  "use strict";
+  const outPlace = $(".js_info-place");
+  const dataInfo = {
+    "120": {
+      title: "Лобовое стекло"
+    },
+    "121": {
+      title: "Дверь"
+    },
+    "122": {
+      title: "Катоп"
+    }
+  };
+  return {
+    init: function() {
+      $(".car__part").click(function(e) {
+        e.preventDefault();
+        const _this = $(this);
+
+        const partID = _this.data("id");
+        const info = dataInfo[partID];
+        if (!info) {
+          return;
+        }
+        outPlace.text(info.title);
+        console.log(info.title);
+      });
+    }
+  };
+})();
+Damages.init();
